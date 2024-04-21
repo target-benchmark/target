3dff40d63f6c3c1d10b62439768f079dce67c811
Carl on Azure VM <jixy2018@gmail.com>
2024-04-21 07:43:18 +0000

    feature: updated a basic generator class

--- generators/DefaultGenerator.py
+++ generators/DefaultGenerator.py
@@ -1,6 +1,28 @@
 from generators.AbsGenerator import AbsGenerator
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage
+from langchain_core.prompts import HumanMessagePromptTemplate
+from langchain_openai import AzureOpenAI
+
 
 class DefaultGenerator(AbsGenerator):
     def __init__(self):
         super().__init__()
+        self.language_model = AzureOpenAI(model="gpt-35-turbo-instruct")
+        self.chat_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(
+                    content=(
+                        "You are a data analyst who reads tables to answer questions."
+                    )
+                ),
+                HumanMessagePromptTemplate.from_template(
+                    "Table: {table_str}\nQuery: {query_str}"
+                ),
+            ]
+        )
+        self.chain = self.chat_template | self.language_model
+
+    def generate(self, table_str: str, query: str) -> str:
+        return self.chain.invoke({"table_str": table_str, "query_str": query})
---
+++ tests/dummy.py
@@ -0,0 +1,23 @@
+import os
+import openai
+
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+)
+
+
+response = client.chat.completions.create(
+  model="gpt-35-turbo-instruct",
+  messages = [{"role":"system","content":"You are an Xbox customer support agent whose primary goal is to help users with issues they are experiencing with their Xbox devices. You are friendly and concise. You only provide factual answers to queries, and do not provide answers that are not related to Xbox."},{"role":"user","content":"How much is a PS5?"}],
+  temperature=0,
+  max_tokens=350,
+  top_p=0.95,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None)
+
+print(response.to_json())
