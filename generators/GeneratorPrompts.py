1947e6c834b3730317177505c2ad081b3b5ae4cf
Carl Ji <jixy2018@gmail.com>
2024-06-26 01:40:52 +0800

    fix: fixing generator for text2sql

--- generators/GeneratorPrompts.py
+++ generators/GeneratorPrompts.py
@@ -8,7 +8,7 @@ QA_USER_PROMPT = "Please use the following table(s) to answer the query. Tables:
 
 ## FOR FACT VERIFICATION TASK ##
 FACT_VER_SYSTEM_PROMPT = "Given the following evidence which may take the form of sentences or a data table, determine whether the evidence supports or refutes the following statement, or does not contain enough information. Assign the statement one of three labels: True, False, Not Enough Information. Do not include anything else in your answer."
-FACT_VER_USER_PROMPT = "Please use the following table(s) to assign the statement the correct label. Tables: {table_str}\Statement: {query_str}"
+FACT_VER_USER_PROMPT = "Please use the following table(s) to assign the statement the correct label. Tables: {table_str}\nStatement: {query_str}"
 
 ## FOR TEXT TO SQL TASK ##
 TEXT2SQL_SYSTEM_PROMPT = "You are an expert and very smart data analyst."
@@ -27,12 +27,9 @@ information regarding relationships or constraints.
 Question:
 {query_str}
 
-Please respond with a JSON object structured as follows:
-{
-"chain_of_thought_reasoning": "Your thought process on how you arrived
-at the final SQL query.",
-"SQL": "Your SQL query in a single string."
-}
+Please respond with a paragraph structured as follows:
+
+\n{format_instructions}
 
 Take a deep breath and think step by step to find the correct SQLite SQL
 query. If you follow all the instructions and generate the correct query,
--- tests/generators_basic_test.py
+++ tests/generators_basic_test.py
@@ -1,6 +1,6 @@
 import unittest
 from generators.DefaultGenerator import DefaultGenerator
-
+from generators.Text2SQLGenerator import Text2SQLGenerater
 
 class TestEvaluator(unittest.TestCase):
     def setUp(self):
@@ -19,6 +19,36 @@ class TestEvaluator(unittest.TestCase):
         answer = self.dg.generate(table_str=table_str, query=query)
         print(answer)
 
+    def test_text2sql_generator(self):
+        table_str = """Table Name: perpetrator
+ Schema:
+CREATE TABLE "perpetrator" (
+"Perpetrator_ID" int,
+"People_ID" int,
+"Date" text,
+"Year" real,
+"Location" text,
+"Country" text,
+"Killed" int,
+"Injured" int,
+PRIMARY KEY ("Perpetrator_ID"),
+FOREIGN KEY ("People_ID") REFERENCES "people"("People_ID")
+)
+Table Name: people
+ Schema:
+CREATE TABLE "people" (
+"People_ID" int,
+"Name" text,
+"Height" real,
+"Weight" real,
+"Home Town" text,
+PRIMARY KEY ("People_ID")
+)
+"""
+        query = "what is the name of the most recently recorded perpetrator?"
+        generator = Text2SQLGenerater()
+        print(generator.generate(table_str, query))
+        
 
 if __name__ == "__main__":
     unittest.main()
