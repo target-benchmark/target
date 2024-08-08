b21be161586cd2c3663c2309b7fcbfda3578c250
Carl Ji <jixy2018@gmail.com>
2024-08-08 11:08:41 +0800

    fix: use duration instead of time

--- evaluators/TARGET.py
+++ evaluators/TARGET.py
@@ -482,8 +482,8 @@ class TARGET:
 
                     # create embedding statistics data object to record latency & size of embedding
                     embedding_stats[dataset_name] = EmbeddingStatisticsDataModel(
-                        embedding_creation_time=round(duration, 5),
-                        avg_embedding_creation_time=round(duration / size_of_corpus, 5),
+                        embedding_creation_duration=round(duration, 5),
+                        avg_embedding_creation_duration=round(duration / size_of_corpus, 5),
                         embedding_size=round(embedding_size, 5),
                         avg_embedding_size=round(embedding_size / size_of_corpus, 5),
                     )
