12a8a592bc9501ae101f83ebec00480e4e500537
Carl Ji <jixy2018@gmail.com>
2024-08-09 02:19:45 +0800

    fix: hacky fix for retrieving with topk. weird that llamaindex retrieversneed to specify top k at creation time instead of retrieval time

--- retrievers/llama_index/LlamaIndexRetriever.py
+++ retrievers/llama_index/LlamaIndexRetriever.py
@@ -28,6 +28,8 @@ class LlamaIndexRetriever(AbsCustomEmbeddingRetriever):
         """
         super().__init__(expected_corpus_format="dataframe")
         self.query_pipelines = {}
+        self.object_indices = {}
+        self.top_ks = {}
 
     def retrieve(
         self,
@@ -36,11 +38,16 @@ class LlamaIndexRetriever(AbsCustomEmbeddingRetriever):
         top_k: int,
         **kwargs,
     ) -> List[Tuple]:
+        if dataset_name not in self.top_ks or top_k != self.top_ks[dataset_name] or dataset_name not in self.query_pipelines:
+            # check if we need to reconstruct new query pipeline
+            # if the top k has changed, or a qp has never been constructed, construct the query pipeline with the new top k
+            self.construct_query_pipeline(dataset_name, top_k)
+        
         retrieved_tables = self.query_pipelines[dataset_name].run(query=query)
         answers = []
         for res in retrieved_tables:
             answers.append(tuple(res.table_name.split(":")[:2]))
-        print(answers)
+        
         return answers
 
     def embed_corpus(self, dataset_name: str, corpus: Iterable[Dict]) -> None:
@@ -83,14 +90,16 @@ class LlamaIndexRetriever(AbsCustomEmbeddingRetriever):
             table_node_mapping,
             VectorStoreIndex,
         )
-        # self.obj_retrievers[dataset_name] = obj_index.as_retriever(similarity_top_k=3)
-
-        qp = QP(verbose=True)
-        qp.add_modules(
+        self.object_indices[dataset_name] = obj_index
+    
+    def construct_query_pipeline(self, dataset_name: str, top_k: int):
+        query_pipeline = QP(verbose=True)
+        query_pipeline.add_modules(
             module_dict={
                 "input": InputComponent(),
-                "table_retriever": obj_index.as_retriever(similarity_top_k=3),
+                "table_retriever": self.object_indices[dataset_name].as_retriever(similarity_top_k=top_k),
             }
         )
-        qp.add_link("input", "table_retriever")
-        self.query_pipelines[dataset_name] = qp
+        query_pipeline.add_link("input", "table_retriever")
+        self.query_pipelines[dataset_name] = query_pipeline
+        self.top_ks[dataset_name] = top_k
