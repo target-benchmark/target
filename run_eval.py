5de64c93292ab0b193033570dadd08d445269168
Madelon Hulsebos <madelon.hulsebos@gmail.com>
2024-09-05 16:09:16 +0200

    update eval script with retrievers

--- run_eval.py
+++ run_eval.py
@@ -4,6 +4,7 @@ import logging
 import time
 import unittest
 
+from retrievers.analysis.NoContextRetriever import NoContextRetriever
 from retrievers.hyse.HySERetriever import HySERetriever
 from retrievers.naive.HNSWOpenAIEmbeddingRetriever import HNSWOpenAIEmbeddingRetriever
 from retrievers.ottqa.OTTQARetriever import OTTQARetriever
@@ -19,6 +20,7 @@ class RetrieverEval:
         retriever_name: str,
         retriever,
         tasks_list: list,
+        out_dir: str,
         result_appendix: str = None,
         persist_log: str = True,
         log_file_path: str = None,
@@ -26,10 +28,7 @@ class RetrieverEval:
         self.retriever_name = retriever_name
         self.result_appendix = result_appendix
 
-        self.out_dir = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            f"retrieval_files/{retriever_name.lower()}",
-        )
+        self.out_dir = out_dir
         if not os.path.exists(self.out_dir):
             os.makedirs(self.out_dir, exist_ok=True)
 
@@ -42,14 +41,14 @@ class RetrieverEval:
         """Runs evaluation and writes results (retrieval, downsream task, total eval time) to a json file."""
         self.target.logger.info(f"starting {self.retriever_name}")
         start = time.time()
-        res = self.target.run(self.retriever, split="validation", top_k=top_k)
+        res = self.target.run(self.retriever, split="validation", top_k=top_k, batch_size=100, debug=False)
         eval_time = time.time() - start
 
         num_schemas_appendix = ""
         if self.retriever_name == "HySE":
             num_schemas_appendix = f"_numschemas_{self.retriever.num_schemas}_withquery_{self.retriever.with_query}"
 
-        if self.retriever_name == "OTTQA":
+        if self.retriever_name in ["OTTQA", "NoContext"]:
             self.retriever.num_rows = ""
 
         res_dict = {}
@@ -70,31 +69,72 @@ if __name__ == "__main__":
     # TODO: make retrieval class keywords+functionality consistent in terms of paths
 
     tasks_list = ["Table Question Answering Task", "Fact Verification Task"]
+    
+    model_name = "openai"
+    out_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/hyse/{model_name}"
+    )
+    for aggregated in [False, True]:
+        for with_query in [False, True]:
+            for num_rows in [0, 2]:
+                for num_schemas in [1, 2, 5]:
+                    hyse_retriever = HySERetriever(
+                        out_dir,
+                        model_name=model_name,
+                        num_rows=num_rows,
+                        num_schemas=num_schemas,
+                        with_query=with_query,
+                        aggregated=aggregated,
+                    )
+                    RetrieverEval(
+                        retriever_name="HySE",
+                        out_dir=out_dir,
+                        retriever=hyse_retriever,
+                        tasks_list=tasks_list,
+                        result_appendix=f"numrows_{num_rows}_numschemas_{num_schemas}_aggregated_{aggregated}_withquery_{with_query}"
+                    ).run_eval()
 
-    for with_query in [False, True]:
-        for num_schemas in [1, 2, 5]:
+    model_name = "tapas"
+    out_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/hyse/{model_name}"
+    )
+    num_rows = 2
+    num_schemas = 1
+    for aggregated in [False, True]:
+        for with_query in [False, True]:
             hyse_retriever = HySERetriever(
-                os.path.join(
-                    os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/hyse"
-                ),
-                num_rows=2,
+                out_dir=out_dir,
+                model_name=model_name,
+                num_rows=num_rows,
+                # num_schemas does not matter
                 num_schemas=num_schemas,
                 with_query=with_query,
+                aggregated=aggregated,
             )
-            RetrieverEval(retriever_name="HySE", retriever=hyse_retriever, tasks_list=tasks_list, result_appendix="agg").run_eval()
-        
-    # for num_rows in [0, 1, 2, 5]:
-    naive_openai_retriever = HNSWOpenAIEmbeddingRetriever(
-        os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/hnswopenai"
-        ),
-        num_rows=2,
+            RetrieverEval(retriever_name="HySE", out_dir=out_dir, retriever=hyse_retriever, tasks_list=tasks_list, result_appendix=f"numrows_{num_rows}_numschemas_{num_schemas}_aggregated_{aggregated}_withquery_{with_query}").run_eval()
+    
+    out_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/hnswopenai"
     )
-    RetrieverEval(retriever_name="HNSWOpenAI", retriever=naive_openai_retriever, tasks_list=tasks_list).run_eval()
+    for num_rows in [0, 2]:
+        naive_openai_retriever = HNSWOpenAIEmbeddingRetriever(
+            out_dir=out_dir,
+            num_rows=num_rows,
+        )
+        RetrieverEval(retriever_name="HNSWOpenAI", out_dir=out_dir, retriever=naive_openai_retriever, tasks_list=tasks_list, result_appendix=f"numrows_{num_rows}").run_eval()
 
-    # OTTQA BM25 retriever
-    ottqa_retriever = OTTQARetriever(encoding="bm25")
-    RetrieverEval(retriever_name="OTTQA", retriever=ottqa_retriever, tasks_list=tasks_list, result_appendix="bm25").run_eval()
+    out_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/ottqa"
+    )
+    # OTTQA retriever
+    for encoding in ["bm25", "tfidf"]:
+        for withtitle in [True]:#, False]:
+            ottqa_retriever = OTTQARetriever(encoding=encoding, out_dir=out_dir, withtitle=withtitle)
+            RetrieverEval(retriever_name="OTTQA", out_dir=out_dir, retriever=ottqa_retriever, tasks_list=tasks_list, result_appendix=f"encoding_{encoding}_withtitle_{withtitle}").run_eval()
 
-    ottqa_retriever = OTTQARetriever(encoding="tfidf")
-    RetrieverEval(retriever_name="OTTQA", retriever=ottqa_retriever, tasks_list=tasks_list, result_appendix="tfidf").run_eval()
+
+    out_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"retrieval_files/analysis"
+    )
+    analysis_nocontext_retriever = NoContextRetriever(out_dir=out_dir)
+    RetrieverEval(retriever_name="NoContext", out_dir=out_dir, retriever=analysis_nocontext_retriever, tasks_list=tasks_list).run_eval()
