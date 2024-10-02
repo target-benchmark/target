f2583c42aa452db9a1d4855dc47ac9fc5c18d1c3
Carl Ji <jixy2018@gmail.com>
2024-10-02 08:54:08 -0700

    fix: fix setup package requirements

--- setup.py
+++ setup.py
@@ -18,41 +18,46 @@ setup(
     license="Apache 2.0",
     python_requires=">=3.8",
     install_requires=[
-        "datasets==2.19.0",
-        "evaluate==0.4.2",
-        "func-timeout==4.3.5",
-        "hnswlib==0.8.0",
-        "langchain==0.1.16",
-        "langchain-community==0.0.34",
-        "langchain-core==0.1.45",
-        "langchain-openai==0.0.8",
-        "langchain-text-splitters==0.0.1",
-        "numpy==1.26.4",
-        "pandas==2.2.2",
-        "pydantic==2.7.1",
-        "python-dateutil==2.9.0",
-        "python-dotenv==1.0.1",
-        "qdrant_client==1.9.1",
-        "regex==2023.10.3",
-        "rouge_score==0.1.2",
-        "sacrebleu==2.4.2",
-        "scikit_learn==1.3.0",
-        "scipy==1.13.0",
-        "setuptools==69.1.1",
-        "transformers==4.41.2",
+        "datasets>=2.19.0",
+        "evaluate>=0.4.2",
+        "func-timeout>=4.3.5",
+        "hnswlib>=0.8.0",
+        "langchain>=0.1.16",
+        "langchain-community>=0.0.34",
+        "langchain-core>=0.1.45",
+        "langchain-openai>=0.0.8",
+        "langchain-text-splitters>=0.0.1",
+        "llama-index>=0.10.58",
+        "nltk>=3.8.1",
+        "numpy>=1.26.4",
+        "pandas>=2.2.2",
+        "pexpect>=4.9.0",
+        "pydantic>=2.7.4",
+        "python-dateutil>=2.9.0",
+        "python-dotenv>=1.0.1",
+        "qdrant_client>=1.9.1",
+        "regex>=2023.10.3",
+        "rouge_score>=0.1.2",
+        "sacrebleu>=2.4.2",
+        "scikit_learn>=1.3.0",
+        "scipy>=1.13.0",
+        "setuptools>=69.1.1",
+        "spacy>=3.7.4",
+        "transformers>=4.41.2",
         "tqdm>=4.65.0",
     ],
     tests_require=[
         "unittest",
     ],
-    extras_require={
-        "llamaindex_retriever": [
-            "llama-index==0.10.58"
-        ],  # Optional dependencies for llama index
-        "ottqa_retriever": [
-            "nltk==3.8.1",
-            "pexpect==4.9.0",
-            "spacy==3.7.4",
-        ],
-    },
+    # TODO: lazy importing for extras
+    # extras_require={
+    #     "llamaindex_retriever": [
+    #         "llama-index>=0.10.58"
+    #     ],  # Optional dependencies for llama index
+    #     "ottqa_retriever": [
+    #         "nltk>=3.8.1",
+    #         "pexpect>=4.9.0",
+    #         "spacy>=3.7.4",
+    #     ],
+    # },
 )
