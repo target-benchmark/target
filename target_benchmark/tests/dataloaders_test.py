4510ea89bb5ee158127032fdf38d76dc32492bc8
Carl on Azure VM <jixy2018@gmail.com>
2024-11-24 21:10:38 +0000

    fix: update test

--- target_benchmark/tests/dataloaders_test.py
+++ target_benchmark/tests/dataloaders_test.py
@@ -9,13 +9,14 @@ from target_benchmark.dataset_loaders.AbsDatasetLoader import QueryType
 class DataloadersTest(unittest.TestCase):
     def setUp(self):
         self.tabfact_loader = HFDatasetLoader(**DEFAULT_TABFACT_DATASET_CONFIG.model_dump())
+        self.tabfact_loader.load()
 
     def test_query_dataset_start_index(self):
         start_idx = 100
         num_queries = 0
         for _ in self.tabfact_loader.get_queries_for_task(start_idx=start_idx, batch_size=1):
             num_queries += 1
-        self.assertEqual(num_queries, self.tabfact_loader.get_queries_size() - num_queries)
+        self.assertEqual(num_queries, self.tabfact_loader.get_queries_size() - start_idx)
 
     def test_factver(self):
         self.assertEqual(self.tabfact_loader.query_type, QueryType.FACT_VERIFICATION)
