c329911b9cec5892c37469867c77781b0090c1e1
Carl Ji <jixy2018@gmail.com>
2024-11-24 13:08:45 -0800

    fix: added test for the new start idx functionality

--- target_benchmark/tests/dataloaders_test.py
+++ target_benchmark/tests/dataloaders_test.py
@@ -7,10 +7,18 @@ from target_benchmark.dataset_loaders.AbsDatasetLoader import QueryType
 
 
 class DataloadersTest(unittest.TestCase):
+    def setUp(self):
+        self.tabfact_loader = HFDatasetLoader(**DEFAULT_TABFACT_DATASET_CONFIG.model_dump())
+
+    def test_query_dataset_start_index(self):
+        start_idx = 100
+        num_queries = 0
+        for _ in self.tabfact_loader.get_queries_for_task(start_idx=start_idx, batch_size=1):
+            num_queries += 1
+        self.assertEqual(num_queries, self.tabfact_loader.get_queries_size() - num_queries)
 
     def test_factver(self):
-        tabfact_loader = HFDatasetLoader(**DEFAULT_TABFACT_DATASET_CONFIG.model_dump())
-        self.assertEqual(tabfact_loader.query_type, QueryType.FACT_VERIFICATION)
+        self.assertEqual(self.tabfact_loader.query_type, QueryType.FACT_VERIFICATION)
 
 
 if __name__ == "__main__":
